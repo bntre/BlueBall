22ec069ec362ecf6b5fd73237959c02cdbb9325f
bntre <massalogin@gmail.com>
2023-02-10 09:04:20 +0200

    BlueBall: teleports

--- game.py
+++ game.py
@@ -1,6 +1,7 @@
 
 import re
 import pygame
+from collections import defaultdict
 
 
 #----------------------------------
@@ -39,22 +40,24 @@ def split_text_to_cell_map(text, cell_width = 4):
 #----------------------------------
 #  
 # [31 30 29 28 27 26 25 24][23 22 21 20 19 18 17 16][15 14 13 12 11 10  9  8][ 7  6  5  4  3  2  1  0]
-#                 [  dir  ;        letter          ]             [\  /  |  -]
-#                 [                                ]             [   RAYS   ]
-#                 [        block name bits         ][                 property bits                  ]
-#                 # used for drawing static objects;
-#                 # 0 for boxes - BIT_BOX used instead;
+#        [ color ;   dir  ;        letter          ]             [\  /  |  -]
+#        [                                         ]             [   RAYS   ]
+#        [            block name bits              ][                 property bits                  ]
+#        # used for drawing static objects;
+#        # 0 for boxes - BIT_BOX used instead;
 
 
-BIT_VISIBLE     = 1     #!!! needed?
-BIT_SOLID       = 1<<1
-BIT_OPAQUE      = 1<<2
-BIT_FINISH      = 1<<3  # to mark finish cell (it stays "invisible")
-BIT_BOX         = 1<<4
-BIT_BOX_BROKEN  = 1<<5
-#BIT_DYNAMIC     = 1<<7  # dynamic object    !!! needed?
-BIT_RAY0        = 1<<8  # shift 0..3 for  --  |  /  \
-BITS_RAYS       = (BIT_RAY0 << 0) | (BIT_RAY0 << 1) | (BIT_RAY0 << 2) | (BIT_RAY0 << 3)
+BIT_VISIBLE          = 1     #!!! needed?
+BIT_SOLID            = 1<<1
+BIT_OPAQUE           = 1<<2
+BIT_FINISH           = 1<<3  # to mark finish cell (it stays "invisible")
+BIT_BOX              = 1<<4
+BIT_BOX_BROKEN       = 1<<5
+BIT_TELEPORT         = 1<<6
+BIT_TELEPORT_SECOND  = 1<<7  # 0|1 - to easy find the second teleport
+#BIT_DYNAMIC          = 1<<7  # dynamic object    !!! needed?
+BIT_RAY0             = 1<<8  # shift 0..3 for  --  |  /  \
+BITS_RAYS            = (BIT_RAY0 << 0) | (BIT_RAY0 << 1) | (BIT_RAY0 << 2) | (BIT_RAY0 << 3)
 
 BITS_WALL       = BIT_VISIBLE | BIT_SOLID | BIT_OPAQUE
 #BITS_FINISH     = BIT_VISIBLE | BIT_NEEDS_BG
@@ -64,6 +67,7 @@ BITS_LASER      = BIT_VISIBLE | BIT_SOLID | BIT_OPAQUE
 BITS_ICELASER   = BIT_VISIBLE | BIT_SOLID
 BITS_BOX        = BIT_VISIBLE | BIT_SOLID | BIT_OPAQUE | BIT_BOX
 #BITS_BOX_BROKEN = BIT_VISIBLE |                          BIT_BOX_BROKEN
+BITS_TELEPORT   = BIT_VISIBLE | BIT_SOLID | BIT_OPAQUE | BIT_TELEPORT
 
 PROPERTY_BITS_MAP = {    # block name letter => property bits
     "W": BITS_WALL,
@@ -73,20 +77,28 @@ PROPERTY_BITS_MAP = {    # block name letter => property bits
     "J": BITS_ICELASER,
     "B": BITS_BOX,
     "A": BITS_WALL,         # Arrow (dynamic blocks)
+    "T": BITS_TELEPORT,
 }
 
-BLOCK_NAME_SHIFT = 16
-BLOCK_NAME_MASK = (0x7FF) << BLOCK_NAME_SHIFT  # 3 bits for direction, 8 bits for a letter
+BLOCK_NAME_SHIFT        = 16
+BLOCK_NAME_SHIFT_DIR    = 16 + 8
+BLOCK_NAME_SHIFT_COLOR  = 16 + 8 + 3
+BLOCK_NAME_MASK         = 0x3FFF << BLOCK_NAME_SHIFT  # 3 bits for color, 3 bits for direction, 8 bits for a letter
+BLOCK_NAME_MASK2        =  0x7FF << BLOCK_NAME_SHIFT  #                   3 bits for direction, 8 bits for a letter
 
-CELLSIZE = 9
+CELLSIZE = 9  # in pixels
 
-
-def block_name_to_pair(name):
-    return name[0], int(name[1:] or "0")
-def block_pair_to_bits(letter, direction):
-    bits  = ord(letter) <<      BLOCK_NAME_SHIFT
-    bits |=   direction << (8 + BLOCK_NAME_SHIFT)
-    return bits
+def block_name_to_tuple(name):
+    return name[0], int(name[1:] or "0"), 0
+def block_tuple_to_bits(letter, direction, color):
+    nameBits  = ord(letter) << BLOCK_NAME_SHIFT
+    nameBits |=   direction << BLOCK_NAME_SHIFT_DIR
+    nameBits |=       color << BLOCK_NAME_SHIFT_COLOR
+    return nameBits
+def get_bits_dir(bits):
+    return (bits >> BLOCK_NAME_SHIFT_DIR) & 0x7
+def get_bits_color(bits):
+    return (bits >> BLOCK_NAME_SHIFT_COLOR) & 0x7
 
 
 def create_image_areas(text, cell_width = 4):
@@ -94,23 +106,23 @@ def create_image_areas(text, cell_width = 4):
     m = split_text_to_cell_map(text, cell_width)
     areas = {}
     for (name,(i,j)) in m.items():
-        nameBits = block_pair_to_bits(*block_name_to_pair(name))
+        nameBits = block_tuple_to_bits(*block_name_to_tuple(name))
         areas[nameBits] = (j*CELLSIZE, i*CELLSIZE, CELLSIZE, CELLSIZE)
     return areas
 
 # block name bits => area
 BLOCK_AREAS = create_image_areas("""
-H0  H1  H2  H3
-H4  H5  H6  H7
-S   W   F   I
-L0  L2  L1  L3
-L6  L5  L7  L4
-B0  B1      
-A7  A5  A4  A6
-A2  A0  A1  A3
-J2  J1  J3  J0
-J6  J5  J7  J4
-R1  R0  R2  R3
+H0  H1  H2  H3                  
+H4  H5  H6  H7                  
+S   W   F   I               T0  
+L0  L2  L1  L3              T1  
+L6  L5  L7  L4              T3  
+B0  B1                      T2  
+A7  A5  A4  A6                  
+A2  A0  A1  A3                  
+J2  J1  J3  J0                  
+J6  J5  J7  J4                  
+R1  R0  R2  R3  C1  C2  C3      
 """)
 
 
@@ -127,16 +139,22 @@ W   W   W   W   W   W
 
 {   'name': "temp", 
     'map': """
-W   W   W   W   W   W   W   W   W   W   W
-W   F       H           i               W
-W                                       W
-L0          J3                  B       W
-W                                       W
-W       I                               L1
-W                       j               W
-W   B   B                   B           W
-W                                       W
-W                                       W
+W   W   W   W   W   W   W   W   W   W   W   W   W
+W   F                   i       T12             W
+W                                               W
+L0          J3                  B               W
+W                                               W
+W       I                                       L1
+W                       j                       W
+W   B   B                   B                   W
+W                   T02                         W
+W           W   H                               W
+W                                               W
+W               B                               W
+W                   T01         T11 B           W
+W                                               W
+W                                               W
+W                                               W
 """,
     'dynamics': [
         ("ij", "A7", 500)
@@ -226,23 +244,23 @@ W   W   W   W   H   W
 
 ] # end of LEVELS
 
-LEVEL_TO_START = "temp"
 LEVEL_TO_START = 5
+LEVEL_TO_START = "temp"
 
 
-KEY_STEPS = {
-    pygame.K_LEFT:  (-1, 0),
-    pygame.K_RIGHT: ( 1, 0),
-    pygame.K_UP:    ( 0,-1),
-    pygame.K_DOWN:  ( 0, 1),
-}
-
-LASER_DIRS = [
+DIRECTIONS = [
     ( 1, 0), (-1, 0),   #  >  <     0  1   >>1  0 --
     ( 0,-1), ( 0, 1),   #  ^  v     2  3        1  |
     ( 1,-1), (-1, 1),   #  /` ./    4  5        2  /
     (-1,-1), ( 1, 1),   #  `\  \,   6  7        3  \
 ]
+
+KEY_DIRECTIONS = {  # key => index of DIRECTIONS
+    pygame.K_RIGHT:  0,
+    pygame.K_LEFT:   1,
+    pygame.K_UP:     2,
+    pygame.K_DOWN:   3,
+}
 
 
 
@@ -264,8 +282,8 @@ class DynamicAnimation(Animation):
 
 class Lazer:
     def __init__(self, direction, staticPos = None, dynamic = None):
-        self.direction = direction  # 0..7  see LASER_DIRS
-        self.staticPos = staticPos  # (x, y) or None for dynamic
+        self.direction = direction  # 0..7  index of DIRECTIONS
+        self.staticPos = staticPos  # (x,y) or None for dynamic
         self.dynamic   = dynamic    # DynamicAnimation for dynamic or None for static
 
 
@@ -288,15 +306,15 @@ class BlueBallGame:
         cellsText = split_text_to_cells(levelDict['map'])
         w, h = len(cellsText[0]), len(cellsText)
         
-        cells = [[None]*w for _ in range(h)]  # [i][j] => (letter, direction) or None
+        cells = [[None]*w for _ in range(h)]  # [i][j] => (letter, direction, color) or None
         aliases = {}                          # cell alias => (i,j)
         
         for (i,row) in enumerate(cellsText):
          for (j,cellText) in enumerate(row):
             if cellText:
-                (letter, d, alias) = re.match(r'^([A-Z]?)([0-9]?)([a-z]?)$', cellText).groups()
+                (letter, direction, color, alias) = re.match(r'^([A-Z]?)([0-9]?)([0-9]?)([a-z]?)$', cellText).groups()
                 if letter:
-                    cells[i][j] = (letter, int(d or "0"))
+                    cells[i][j] = (letter, int(direction or "0"), int(color or "0"))
                 if alias:
                     aliases[alias] = (j,i)
         
@@ -325,6 +343,7 @@ class BlueBallGame:
         self.boxes = []  # list of positions
         self.lazers = []  # list of Lazer objects
         self.lazersOn = True  # we turn them off when hero reach the finish
+        self.teleports = defaultdict(list)  # teleport color => [(x,y), (x,y)] - both teleport poses
         
         self.animations = []  # list of Animation objects
         self.currentFrameDynamics = []  # used to process all current frame dynamic animations at once
@@ -332,24 +351,31 @@ class BlueBallGame:
         # Fill level cell integers
         self.level = [[0]*w for _ in range(h)]  # cell integer values
         for (i,row) in enumerate(mapCells):
-         for (j,blockPair) in enumerate(row):
-            if blockPair:
-                letter, direction = blockPair
+         for (j,blockValues) in enumerate(row):
+            if blockValues:
+                letter, direction, color = blockValues
                 cell = 0
+                addNameBits = True
                 if letter == "H":           # Hero
                     self.heroPos = (j, i)
                 else:
                     if letter == "F":       # Finish: special case - draw by BIT_FINISH
                         self.finishPos = (j, i)
                         cell |= BIT_FINISH
+                        addNameBits = False
                     elif letter == "B":     # Box: special case - draw by BIT_BOX
                         self.boxes.append((j, i))
                         cell |= BIT_BOX
+                        addNameBits = False
                     elif letter in "LJ":    # Laser or Ice laser
                         lazer = Lazer(direction, (j, i))
                         self.lazers.append(lazer)
-                    if cell == 0:  # no special bits were set - means regular static object - add block name bits for drawing
-                        cell |= block_pair_to_bits(letter, direction)
+                    elif letter == "T":     # Teleport
+                        ts = self.teleports[color]
+                        if ts: cell |= BIT_TELEPORT_SECOND  # 1 for the second teleport of this color
+                        ts.append((j, i))
+                    if addNameBits:  # no special bits were set - means regular static object - add block name bits for drawing
+                        cell |= block_tuple_to_bits(letter, direction, color)
                     cell |= PROPERTY_BITS_MAP.get(letter, 0)
                 self.level[i][j] = cell
 
@@ -357,9 +383,9 @@ class BlueBallGame:
         for (poses, blocks, delay) in dynamics:
             self.start_dynamic_animation(poses, blocks, delay)
         
-        self.recalculateLazerRays = True  # set if we need to update the scene - recalculate lazer rays
+        self.recalculateLazerRays = True  # recalculate lazer ray bits
         self.redrawScene = True
-        self.canPlay = True  # False on level ending (just waiting for final animations end)
+        self.canPlay = True  # False on level ending (when just waiting for final animations end)
     
     def start_dynamic_animation(self, keyPoses, blocks, delay):
         poses = []  # collect all poses (from key poses)
@@ -375,9 +401,9 @@ class BlueBallGame:
         # Now start dynamic for each block in 2d array
         for (i,row) in enumerate(blocks):
          for (j,blockName) in enumerate(row):
-            (letter, direction) = block_name_to_pair(blockName)
+            (letter, direction, color) = block_name_to_tuple(blockName)
             d = self.start_dynamic_block(
-                block_pair_to_bits(letter, direction),
+                block_tuple_to_bits(letter, direction, color),
                 PROPERTY_BITS_MAP[letter],
                 [add(p, (j,i)) for p in poses], 
                 delay
@@ -392,9 +418,17 @@ class BlueBallGame:
         w, h = self.levelSize
         return 0 <= x < w and 0 <= y < h
     
-    def draw_block(self, pos, letter = None, direction = 0, nameBits = None):
-        if nameBits is None: 
-            nameBits = block_pair_to_bits(letter, direction)
+    def draw_block(self, pos, letter, direction = 0, color = 0):
+        nameBits = block_tuple_to_bits(letter, direction, color)
+        self.draw_block_by_bits(pos, nameBits)
+        
+    def draw_block_by_bits(self, pos, nameBits):
+        # draw background color if needed
+        color = get_bits_color(nameBits)
+        if color:
+            self.draw_block(pos, "C", direction = color)  # we use direction for color index here
+            nameBits = nameBits & BLOCK_NAME_MASK2  # remove color bits
+        # draw the block (dir and letter bits only)
         area = BLOCK_AREAS.get(nameBits)
         if area:
             self.levelSurface.blit(
@@ -421,16 +455,17 @@ class BlueBallGame:
             # Draw by block name (e.g. L1,..)
             nameBits = cell & BLOCK_NAME_MASK
             if nameBits:
-                self.draw_block((j,i), nameBits = nameBits)
+                self.draw_block_by_bits((j,i), nameBits)
         
         # hero
-        self.draw_block(self.heroPos, "H", self.heroState)
+        if self.heroState < 8:  #!!! 8 for hidden
+            self.draw_block(self.heroPos, "H", self.heroState)
         
         # dynamic objects
         for a in self.animations:
             if a.name == "dyn":
                 pos = a.poses[a.posIndex]
-                self.draw_block(pos, nameBits = a.nameBits)
+                self.draw_block_by_bits(pos, a.nameBits)
 
         # Layer 2
         for (i,a) in enumerate(self.level):
@@ -451,23 +486,21 @@ class BlueBallGame:
 
 
     def recalculate_lazer_rays(self):
+        # Update lazer ray bits (mask: BITS_RAYS)
         w, h = self.levelSize
-        
-        # Update lazers (corresponding cell bits: BITS_RAYS)
         for i in range(h):
          for j in range(w):
             self.level[i][j] &= ~BITS_RAYS;
         if self.lazersOn:
             for lazer in self.lazers:
                 ray = lazer.direction >> 1  # 0..3: - | / \
-                step = LASER_DIRS[lazer.direction]
+                step = DIRECTIONS[lazer.direction]
                 pos = lazer.staticPos or lazer.dynamic.poses[lazer.dynamic.posIndex]
                 while True:
                     pos = (x,y) = add(pos, step)
                     if not self.in_level(pos): break
                     if self.level[y][x] & BIT_OPAQUE: break
                     self.level[y][x] |= BIT_RAY0 << ray;
-    
     
     # Animations
     def process_animations(self):
@@ -513,8 +546,8 @@ class BlueBallGame:
             self.redrawScene = True
             a.time += 100  # request next frame
         else:
-            self.start_new_level_animation()  # reload same level
             a.ended = True
+            self.start_new_level_animation()  # reload same level
 
     # starting new level (1 sec pause)
     def start_new_level_animation(self):
@@ -568,30 +601,52 @@ class BlueBallGame:
         self.recalculateLazerRays = True
         self.redrawScene = True
 
-
-    def handle_hero_step(self, step):
-        """step is e.g. (-1,0)"""
-        newPos = (x,y) = add(self.heroPos, step)
-        #print(step, newPos)
-        if not self.in_level(newPos): return
+    
+    def get_next_cell(self, pos, dirIndex, allowTeleport = True):
+        newPos = (x,y) = add(pos, DIRECTIONS[dirIndex])
+        if not self.in_level(newPos): return (None,None,None)
         bits = self.level[y][x]
+        
+        if allowTeleport:
+            while bits & BIT_TELEPORT:  # there may be few steps like    -> [0 ]     [ 0][O ]     [ O] ->
+                d = get_bits_dir(bits)
+                c = get_bits_color(bits)
+                if d == dirIndex:  # can enter the teleport
+                    i = (bits & BIT_TELEPORT_SECOND) and 1 or 0
+                    (xT,yT) = self.teleports[c][i ^ 1]  # pos of other teleport
+                    bitsT = self.level[yT][xT]
+                    dirIndex = get_bits_dir(bitsT) ^ 1  # step out
+                    newPos = (x,y) = add((xT,yT), DIRECTIONS[dirIndex])
+                    if not self.in_level(newPos): return (None,None,None)
+                    bits = self.level[y][x]
+        
+        return (newPos, bits, dirIndex)
 
+
+    def handle_hero_step(self, dirIndex):
+        
+        newPos, bits, dirIndex = self.get_next_cell(self.heroPos, dirIndex)
+        if newPos is None: return
+        
         if not bits & BIT_SOLID:
             self.heroPos = newPos
-            self.recalculateLazerRays = True
+            #self.recalculateLazerRays = True
             self.redrawScene = True
         
-        if bits & BIT_BOX:  # box. can we move it?
-            newPos2 = (x2,y2) = add(newPos, step)
-            if self.in_level(newPos2):
-                bits2 = self.level[y2][x2]
-                if not bits2 & BIT_SOLID:  # we can move it
-                    self.level[y ][x ] &= ~BITS_BOX  #!!! remove bits by global mask (not only BOX)
-                    self.level[y2][x2] |=  BITS_BOX
-                    self.heroPos = newPos
-                    self.recalculateLazerRays = True
-                    self.redrawScene = True
-                    self.start_pushing_box_animation()
+        elif bits & BIT_BOX:  # box. can we move it?
+            newPos2, bits2, dirIndex2 = self.get_next_cell(newPos, dirIndex)
+            if newPos2 is None: return
+            
+            if not bits2 & BIT_SOLID:  # we can move it
+                (x, y ) = newPos
+                (x2,y2) = newPos2
+                self.level[y ][x ] &= ~BITS_BOX  #!!! remove bits by global mask (not only BOX)
+                self.level[y2][x2] |=  BITS_BOX
+                self.heroPos = newPos
+                self.recalculateLazerRays = True
+                self.redrawScene = True
+                self.start_pushing_box_animation()
+        
     
     def end_playing(self, win):
         if not self.canPlay: return  # already ended
@@ -628,9 +683,9 @@ class BlueBallGame:
                 # window size?
                 elif e.type == pygame.KEYDOWN:
                     if self.canPlay:
-                        step = KEY_STEPS.get(e.key)  # e.g. (-1,0)
-                        if step:
-                            self.handle_hero_step(step)
+                        dirIndex = KEY_DIRECTIONS.get(e.key, -1)  # 0..3
+                        if dirIndex != -1:
+                            self.handle_hero_step(dirIndex)
 
             # Process all animations (remove ended ones)
             self.process_animations()  # animation procs called here
